b06d69c364d3e6a2203113d55085fd26da14f7d2
philiscoding <produktivemail@gmail.com>
2024-06-30 14:27:30 +0200

    Fix bugs with operations, refactor setValues
    Calculator now works mostly correctly, except with
    0, setValues was refactored to setNumbers and
    setOperators for cleaner code.

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-/*Operators */
+/*OPERATIONS */
 function add(a, b) {
     return a + b;
 }
@@ -15,79 +15,87 @@ function divide(a, b) {
     return a / b;
 }
 
-function pow(a, b) {
-    return a ** b;
-}
-
 function operate(a, b, operator) {
     return operator(a, b);
 }
-
 
 /*VARIABLES */
 let firstNum;
 let secondNum;
 let operator;
 let displayValue;
-let displayText = "";
 
-
-
-//if second number does not exist & last item of display
-//is operator, do nothing
-//when = is clicked -> perform calculation
-//if there is only a number and equal is clicked -> do nothing
-//if there is a number and operator -> do nothing
-
-
-
-
-//whenever a number is clicked -> update display
+/*DOM ELEMENTS*/
+const buttons = document.querySelectorAll("button");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const display = document.querySelector(".display");
 const equal = document.querySelector(".equal");
 
-//whenever a number is clicked -> update display
-numbers.forEach(number => {
-    number.addEventListener("click", updateDisplay, );
-});
+/*update display*/
+buttons.forEach(button => {
+    button.addEventListener("click", updateDisplay);
+})
+
+/*update display*/
+equal.addEventListener("click", updateDisplay);
 
 //set number values to do math
 numbers.forEach(number => {
-    number.addEventListener("click", setValues)
-});
-
-
-//whenever operator is clicked -> update display
-operators.forEach(operator => {
-    operator.addEventListener("click", updateDisplay)
+    number.addEventListener("click", setNumbers)
 });
 
 //set operator value to do math
 operators.forEach(operator => {
-    operator.addEventListener("click", setValues)
+    operator.addEventListener("click", setOperator)
 });
 
-equal.addEventListener("click", updateDisplay);
 
 function updateDisplay(e) {
     let clickedBtn = e.target;
-    if (clickedBtn.dataset.attribute != "equal") {
-        displayText += e.target.textContent;
-        display.textContent = displayText;
+
+    if (!displayValue) {
+        displayValue = clickedBtn.textContent;
+        display.textContent = displayValue;
+    } else if (clickedBtn.dataset.attribute == "number" ||
+        clickedBtn.dataset.attribute == "operator") {
+        displayValue += clickedBtn.textContent;
+        display.textContent = displayValue;
     }
 
-
+    /* press equal sign */
     if (clickedBtn.dataset.attribute == "equal") {
-        display.textContent = performOperation();
+        if (firstNum && secondNum && operator) {
+            displayValue = performOperation();
+            display.textContent = displayValue;
+            console.log(`displayValue: ${displayValue}`);
+            console.log(`first num: ${firstNum}`);
+            console.log(`operator: ${operator}`);
+        }
+    }
+
+    if (firstNum && operator && clickedBtn.dataset.attribute == "operator") {
+        displayValue = displayValue.slice(0, displayValue.length - 2) + clickedBtn.textContent;
+        display.textContent = displayValue;
+    }
+
+    if (clickedBtn.dataset.attribute == "operator" && firstNum && secondNum && operator) {
+        displayValue = performOperation() + clickedBtn.textContent;
+        display.textContent = displayValue;
+    }
+
+    if (clickedBtn.dataset.attribute ==
+        "number" &&
+        operator &&
+        firstNum &&
+        secondNum) {
+        display.textContent = displayValue;
     }
 }
 
 function performOperation() {
     if (firstNum && secondNum && operator) {
         let result = operate(firstNum, secondNum, operator);
-        displayValue = result;
         firstNum = result;
         secondNum = null;
         operator = null;
@@ -95,33 +103,35 @@ function performOperation() {
     }
 }
 
-function resetValues() {
-
-}
-
-//sets firstNum, secondNum, operator
-function setValues(e) {
+function setNumbers(e) {
     let clickedBtn = e.target;
 
-    //Sets display values in order to get numbers later
-    if (displayValue === undefined) {
-        displayValue = +clickedBtn.textContent;
-    } else {
-        displayValue += clickedBtn.textContent;
+    //sets display if it was empty before
+    if (!displayValue) {
+        displayValue = clickedBtn.textContent;
+        display.textContent = displayValue;
     }
 
     //Sets first and second number
     if (clickedBtn.dataset.attribute == "number") {
         if (!operator) {
-            firstNum = +displayValue;
+            firstNum = parseInt(displayValue);
+            console.log(`displayValue: ${displayValue}`);
             console.log(`first num: ${firstNum}`);
             console.log(`operator: ${operator}`);
+
         } else {
             secondNum = +displayValue.slice(firstNum.toString().length + 1);
-            console.log("length:" + firstNum.length);
+            console.log(`displayValue: ${displayValue}`);
             console.log(`first num: ${firstNum}, operator: ${operator.toString()}, second num: ${secondNum}`);
         }
     }
+
+
+}
+//sets operator
+function setOperator(e) {
+    let clickedBtn = e.target;
 
     //Sets operator if operator if operator button is clicked
     if (clickedBtn.dataset.attribute == "operator") {
@@ -139,6 +149,7 @@ function setValues(e) {
                 operator = divide;
                 break;
         }
-        console.log(operator);
+        console.log(`displayValue: ${displayValue}`);
+        console.log(`first num: ${firstNum}, operator: ${operator.toString()}, second num: ${secondNum}`);
     }
 }
